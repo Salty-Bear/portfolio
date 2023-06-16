0a6f9573673f142830e9a0a166f0d89590d0484c
Aryaman <aryamanraj123456789@gmail.com>
2023-06-16 10:11:11 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { appRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -18,7 +23,11 @@ import { appRoutingModule } from './app-routing.module';
     BrowserModule,
     appRoutingModule,
     RouterModule,
-    appRoutingModule
+    appRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'portfolio-f5c42',
+    appId: '1:56700823369:web:c33467ba32c76008535c98',
+    storageBucket: 'portfolio-f5c42.appspot.com',
+    apiKey: 'AIzaSyDqQpH_pWYL1DM9LLOGNQOBUfvwW-H1wwE',
+    authDomain: 'portfolio-f5c42.firebaseapp.com',
+    messagingSenderId: '56700823369',
+    measurementId: 'G-G8S3M4WLKY',
+  },
+};
